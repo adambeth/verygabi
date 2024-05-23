0aaf9bbd08a1e067536e509a70a8f1822557a39d
Adam Bethlehem <30418746+adambeth@users.noreply.github.com>
2024-05-23 07:43:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+function NavButtons() {
+  return <></>;
+}
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <h1>💅🏻 VERYGABI 💅🏻</h1>
+      </div>
+    </>
+  );
+}
+export default App;
